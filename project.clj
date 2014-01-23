f04990c2584b13585e2ffc7a63f6a33da551bea2
David Zaharee <david@zaharee.com>
2014-01-23 13:29:13 -0500

    add datomic and instaparse dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/dzaharee/monocular"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.9.4470"]
+                 [instaparse "1.2.14"]])
