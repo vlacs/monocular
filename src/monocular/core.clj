a4c53200c2e24c9018c00ba5ca4d9db188e34d5f
David Zaharee <david@zaharee.com>
2014-01-27 12:19:21 -0500

    create parser for searches

--- src/monocular/core.clj
+++ src/monocular/core.clj
@@ -1,6 +1,15 @@
-(ns monocular.core)
+(ns monocular.core
+  (:require [instaparse.core :as insta]
+            [datomic.api :as d]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def search-parser
+  (insta/parser
+    "search = term (<whitespace> term)*                               (* searches are one or more terms, separated with whitespace *)
+     term = key? value                                                (* terms are values or key:value pairs *)
+     key = #'[\\w\\*\\+\\!\\-\\?\\.]*' <':'>                          (* keys are word (and some allowed symbol) characters ending with a colon *)
+     value = quoted-value | raw-value                                 (* values are raw or quoted... *)
+     <quoted-value> = single-quoted-value | double-quoted-value       (* ... with single or double quotes *)
+     <single-quoted-value> = <'\\''> #'[^\\']*' <'\\''>
+     <double-quoted-value> = <'\"'> #'[^\"]*' <'\"'>
+     <raw-value> = #'[^\"\\':\\s]+'
+     whitespace = #'\\s+'"))
