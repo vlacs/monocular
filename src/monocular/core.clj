428cf678d1aa9ef08dccb2a78c45e9c5d65f1c41
David Zaharee <david@zaharee.com>
2014-02-06 10:18:57 -0500

    start building searcher and search functions

--- src/monocular/core.clj
+++ src/monocular/core.clj
@@ -1,9 +1,10 @@
 (ns monocular.core
-  (:require [instaparse.core :as insta]
+  (:require [monocular.data-map :refer [map->grammar]]
+            [instaparse.core :as insta]
             [instaparse.combinators :refer [ebnf]]
             [datomic.api :as d]))
 
-(def base-gramamr
+(def base-grammar
   (ebnf
     "search = term (<whitespace> term)*
      default = value
@@ -12,6 +13,15 @@
      <quoted-value> = <'\"'> #'[^\"]*' <'\"'>
      whitespace = #'\\s+'"))
 
+;; this will do more than just create an instaparser parser in the future
+(defn searcher
+  [data-map]
+  (insta/parser (merge base-grammar (map->grammar data-map)) :start :search))
+
+;; ..and this will do more than just pass things to instaparse.core/parse
+(defn search
+  [searcher string]
+  (insta/parse searcher string))
 
 ; todo: rework these to match our plan
 ;
@@ -31,5 +41,3 @@
 ;    (d/q query db (into (insta/transform transform parse-tree) rules))))
 
 
-; this is how we make our parser:
-;  (insta/parser (merge base-grammar (map->grammar <insert data map here>)) :start :search)
